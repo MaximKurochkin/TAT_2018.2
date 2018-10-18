3b7afff68f2f5e36962f7b15465b507fc0201214
Max Kurochkin <maks-bereznev@mail.ru>
2018-10-18 04:59:20 +0300

    File upload

---
+++ DEV-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace task_1_10._18._2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = args[0];
+            int sizeOfSequence = 0;
+            StringBuilder sequence = new StringBuilder();
+
+            Console.WriteLine(str);
+
+            switch (str.Length)
+            {
+                case 0:
+                    sizeOfSequence = 0;
+                    break;
+
+                case 1:
+                    sizeOfSequence = 1;
+                    break;
+
+                default:
+
+                    for (int i = 1; i < str.Length; i++)
+                    {
+                        if (i == 1)
+                        {
+                            sequence.Append(str[i - 1]);
+                            sizeOfSequence = 1;
+                        }
+                        if (str[i] != str[i - 1] && !sequence.ToString().Contains(str[i]))
+                        {
+                            sequence.Append(str[i]);
+                        }
+                        else
+                        {
+                            sizeOfSequence = Math.Max(sequence.Length, sizeOfSequence);
+                            sequence.Append(str[i]);
+                            sequence = sequence.Remove(0, sequence.ToString().IndexOf(str[i]) + 1);
+                        }
+                    }
+                    if (sizeOfSequence < sequence.Length) sizeOfSequence = sequence.Length;
+                    break;
+            }
+            Console.WriteLine(sizeOfSequence);
+            Console.ReadKey();
+        }
+    }
+}
