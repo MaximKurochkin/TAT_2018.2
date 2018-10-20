fa9ea0ea354e6528e595de7e20e1bc17d2476beb
Max Kurochkin <maks-bereznev@mail.ru>
2018-10-21 01:03:50 +0300

    Now can translate all sounds from latin to rus.

--- DEV-2/DEV-2/Program.cs
+++ DEV-2/DEV-2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace DEV_2
@@ -11,16 +7,16 @@ namespace DEV_2
     {
         static void Main(string[] args)
         {
-            string str = "privet";
+            //to solve some proplems with translate latin symbols to rus i decide to change same pairs
+            //ж - zhh, и - i, х - khh, ц - c, ч - chh, ш - shh, щ - js, ъ - hhh, ы - ihh, ь - jh, э - ehh, ю - ju, я -ja
+
+            //for translate rus symbols to latin i use the table that you recomend
+            string str = args[0];
             Transliter translit = new Transliter();
             str = translit.Translate(str);
             Console.WriteLine(str);
-
-            string str1 = "привет";
-            Transliter translit1 = new Transliter();
-            str1 = translit1.Translate(str1);
-            Console.WriteLine(str1);
             Console.ReadKey();
+
         }
     }
 }
--- DEV-2/DEV-2/Transliter.cs
+++ DEV-2/DEV-2/Transliter.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DEV_2
 {
+    //class Transliter translate latin symbols to rus and rus symbols to latin
     public class Transliter
     {
         private int languge = 0; // 0 - rus to latin, 1- latin to rus
+
+        //create dictionary for translate rus symbols to latin
+        //use this srtucture becouse russian sounds single characters
         Dictionary<string, string> translitTableRusToLatin = new Dictionary<string, string>()
         {
             {"а", "a"},
@@ -38,100 +40,130 @@ namespace DEV_2
             {"ч", "ch"},
             {"ш", "sh"},
             {"щ", "sch"},
-            {"ъ", ""},
+            {"ъ", "hhh"},
             {"ы", "y"},
-            {"ь", ""},
+            {"ь", "jh"},
             {"э", "e"},
             {"ю", "yu"},
             {"я", "ya"},
         };
 
-        Dictionary<string, string> translitTableLatinToRus = new Dictionary<string, string>()
-        {
-            {"a", "а"},
-            {"b", "б"},
-            {"d", "д"},
-            {"e", "e"},
-            {"f", "ф"},
-            {"g", "г"},
-            {"i", "и"},
-            {"k", "к"},
-            {"l", "л"},
-            {"m", "м"},
-            {"n", "н"},
-            {"o", "о"},
-            {"p", "п"},
-            {"r", "р"},
-            {"s", "с"},
-            {"t", "т"},
-            {"u", "у"},
-            {"v", "в"},
-            {"y", "ы"},
-            {"z", "з"},
-            {"yo", "ё"},
-            {"zh", "ж"},
-            {"kh", "х"},
-            {"ts", "ц"},
-            {"ch", "ч"},
-            {"sh", "ш"},
-            {"sch", "щ"},
-            {"yu", "ю"},
-            {"ya", "я"},
-        };
-
-
-
-
+        //define the language
+        //parameter str - is our string which we need to translate
         public int CheckLanguage(string str)
         {
+            // 0 - rus to latin, 1- latin to rus
             if (str[0] >= 'а') languge = 0;
             else languge = 1;
             return languge;
         }
-        public string Translate (string str)
+
+        //in this method we decide how to translate: 
+        //from russian to latin or from latin to russian
+        //parameter str - is our string which we need to translate
+        public string Translate(string str)
         {
             if (CheckLanguage(str) == 0) return TranslitRusToLatin(str);
             else return TranslitLatinToRus(str);
         }
 
+        
         public string TranslitRusToLatin(string str)
         {
             var result = "";
-            // проход по строке для поиска символов подлежащих замене которые находятся в словаре dictionaryChar
-            foreach (var ch in str)
+            foreach (var ch in str) 
             {
                 var ss = "";
-                // берём каждый символ строки и проверяем его на нахождение его в словаре для замены,
-                // если в словаре есть ключ с таким значением то получаем true 
-                // и добавляем значение из словаря соответствующее ключу
+                //if in dictionary there is key with such value we get true
+                //and add value from dictionary
                 if (translitTableRusToLatin.TryGetValue(ch.ToString(), out ss))
                 {
                     result += ss;
                 }
-                // иначе добавляем тот же символ
+                // or add the same symbol
                 else result += ch;
             }
             return result;
         }
 
-        public string TranslitLatinToRus(string str)
+
+        public string TranslitLatinToRus(string s)
         {
-            var result = "";
-            // проход по строке для поиска символов подлежащих замене которые находятся в словаре dictionaryChar
-            foreach (var ch in str)
+            StringBuilder sb = new StringBuilder(s.Length);
+            int i = 0;
+            while (i < s.Length)
             {
-                var ss = "";
-                // берём каждый символ строки и проверяем его на нахождение его в словаре для замены,
-                // если в словаре есть ключ с таким значением то получаем true 
-                // и добавляем значение из словаря соответствующее ключу
-                if (translitTableLatinToRus.TryGetValue(ch.ToString(), out ss))
-                {
-                    result += ss;
+                char ch = s[i];
+                if (ch == 'j')
+                { 
+                    i++; 
+                    ch = s[i];
+                    switch (ch)
+                    {
+                        //je - ё
+                        //js - щ
+                        case 'e': sb.Append('ё'); break;
+                        case 's':
+                            sb.Append('щ');
+                            i++; 
+                            if (s[i] != 'н') throw new Exception("Illegal transliterated symbol at position " + i);// вариант третьего символа только один
+                            break;
+                        //jh - ь
+                        //ju - ю
+                        //ja - я
+                        case 'h': sb.Append('ь'); break;
+                        case 'u': sb.Append('ю'); break;
+                        case 'a': sb.Append('я'); break;
+                        default: throw new Exception("Illegal transliterated symbol at position " + i);
+                    }
                 }
-                // иначе добавляем тот же символ
-                else result += ch;
+                else if (i + 1 < s.Length && s[i + 1] == 'h' && !(i + 2 < s.Length && s[i + 2] == 'h'))
+                {
+                    switch (ch)
+                    { 
+                        case 'z': sb.Append('ж'); break; //zhh - ж
+                        case 'k': sb.Append('х'); break; //khh - х
+                        case 'c': sb.Append('ч'); break; //chh - ч
+                        case 's': sb.Append('ш'); break; //shh - ш
+                        case 'e': sb.Append('э'); break; //ehh - э
+                        case 'h': sb.Append('ъ'); break; //hhh - ъ
+                        case 'i': sb.Append('ы'); break; //ihh - ы
+                        default: throw new Exception("Illegal transliterated symbol at position " + i);
+                    }
+                    i++;
+                }
+                else
+                {
+                    switch (ch)
+                    {
+                        case 'a': sb.Append('а'); break;
+                        case 'b': sb.Append('б'); break;
+                        case 'v': sb.Append('в'); break;
+                        case 'g': sb.Append('г'); break;
+                        case 'd': sb.Append('д'); break;
+                        case 'e': sb.Append('е'); break;
+                        case 'z': sb.Append('з'); break;
+                        case 'i': sb.Append('и'); break;
+                        case 'y': sb.Append('й'); break;
+                        case 'k': sb.Append('к'); break;
+                        case 'l': sb.Append('л'); break;
+                        case 'm': sb.Append('м'); break;
+                        case 'n': sb.Append('н'); break;
+                        case 'o': sb.Append('о'); break;
+                        case 'p': sb.Append('п'); break;
+                        case 'r': sb.Append('р'); break;
+                        case 's': sb.Append('с'); break;
+                        case 't': sb.Append('т'); break;
+                        case 'u': sb.Append('у'); break;
+                        case 'f': sb.Append('ф'); break;
+                        case 'c': sb.Append('ц'); break;
+                        default: sb.Append(ch); break;
+                    }
+                }
+
+                i++;
             }
-            return result;
+            return sb.ToString();
         }
     }
 }
