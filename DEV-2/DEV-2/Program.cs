49770fcda83614856a9bc6c2181950192fa50e28
Max Kurochkin <maks-bereznev@mail.ru>
2018-10-20 19:21:02 +0300

    Create class Transliter. Can translate rus simbols to latin.

---
+++ DEV-2/DEV-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DEV_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "щетка";
+            Transliter translit = new Transliter();
+            str = translit.TranslitRusToLatin(str);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DEV-2/DEV-2/Transliter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_2
+{
+    public class Transliter
+    {
+        Dictionary<string, string> translitTable = new Dictionary<string, string>()
+        {
+            {"а", "a"},
+            {"б", "b"},
+            {"в", "v"},
+            {"г", "g"},
+            {"д", "d"},
+            {"е", "e"},
+            {"ё", "yo"},
+            {"ж", "zh"},
+            {"з", "z"},
+            {"и", "i"},
+            {"й","y"},
+            {"к", "k"},
+            {"л", "l"},
+            {"м", "m"},
+            {"н", "n"},
+            {"о", "o"},
+            {"п", "p"},
+            {"р", "r"},
+            {"с", "s"},
+            {"т", "t"},
+            {"у", "u"},
+            {"ф", "f"},
+            {"х", "kh"},
+            {"ц", "ts"},
+            {"ч", "ch"},
+            {"ш", "sh"},
+            {"щ", "sch"},
+            {"ъ", ""},
+            {"ы", "y"},
+            {"ь", ""},
+            {"э", "e"},
+            {"ю", "yu"},
+            {"я", "ya"},
+        };
+
+        public string TranslitRusToLatin(string str)
+        {
+            var result = "";
+            // проход по строке для поиска символов подлежащих замене которые находятся в словаре dictionaryChar
+            foreach (var ch in str)
+            {
+                var ss = "";
+                // берём каждый символ строки и проверяем его на нахождение его в словаре для замены,
+                // если в словаре есть ключ с таким значением то получаем true 
+                // и добавляем значение из словаря соответствующее ключу
+                if (translitTable.TryGetValue(ch.ToString(), out ss))
+                {
+                    result += ss;
+                }
+                // иначе добавляем тот же символ
+                else result += ch;
+            }
+            return result;
+        }
+     
+    }
+}
