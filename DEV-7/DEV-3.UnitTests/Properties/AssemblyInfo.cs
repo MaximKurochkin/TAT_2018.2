a4aa3a946ef344a2d80d254164dac7f0621b16d1
Max Kurochkin <maks-bereznev@mail.ru>
2018-11-23 16:36:10 +0300

    Created unit-tests for application DEV-3.

---
+++ DEV-7/DEV-3.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DEV-3.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DEV-3.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e199445f-f503-4722-8e2c-f4a3eaef2393")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DEV-7/DEV-3/ConvertFromOneNumberSystemToAnother.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace DEV_3
+{
+    // <summary>
+    // TranslateFromOneNumberSystemToAnother
+    // Translates numeric from one number system
+    // to another with radix from 2 to 20
+    // </summary>
+    class ConvertFromOneNumberSystemToAnother
+    {
+        // entery point into programm
+        static void Main(string[] args)
+        {
+            try
+            {
+                NumericConverterToNewNumberSystem convertor = new NumericConverterToNewNumberSystem(args[0], args[1]);
+                string afterConvertion = convertor.ConvertInto();
+                Console.WriteLine(" Your numeric in number system with base 10: " + args[0]);
+                Console.WriteLine(" Your nameric in number system with base " + args[1] + ": " + afterConvertion);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
