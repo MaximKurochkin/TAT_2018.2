3f1120c98eb365191720f7e486f9fba3f7fba38f
Max Kurochkin <maks-bereznev@mail.ru>
2018-12-08 12:53:38 +0300

    Task-1

---
+++ task-1/task-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    /// <summary>
+    /// Class Program
+    /// prints count of the same sumbols in user string
+    /// </summary>
+    
+    class Program
+    {
+        /// <summary>
+        /// Method Main
+        /// entery point
+        /// </summary>
+        /// <param name="args">user string</param>
+    
+        static void Main(string[] args)
+        {
+            CountOfTheSameSymbols count = new CountOfTheSameSymbols(args[0]);
+            count.PrintCountOfTheSameSymbols();
+
+        }
+    }
+}
