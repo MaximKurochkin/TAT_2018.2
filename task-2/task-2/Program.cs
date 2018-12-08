cd717685defd5b1ab971145d764bbb9addf501b8
Max Kurochkin <maks-bereznev@mail.ru>
2018-12-08 13:03:05 +0300

    Task-2

---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    /// <summary>
+    /// Class Program
+    /// founds ip-adress in input string
+    /// </summary>
+    
+    class Program
+    {
+        /// <summary>
+        /// Method Main
+        /// Entery Point
+        /// </summary>
+        /// <param name="args">input string</param>
+        
+        static void Main(string[] args)
+        {
+            FinderOfIPinStr finder = new FinderOfIPinStr();
+            List<string> listOfIP = new List<string>();
+            listOfIP = finder.GetIPFromStr(args[0]);
+
+            foreach(string ip in listOfIP)
+            {
+                Console.WriteLine("IP-adress is " +ip);
+            }
+        }
+    }
+}
